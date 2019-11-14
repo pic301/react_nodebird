bba9216b4511cd6f81dd970309a0dbfa39f9529b
pic301 <ppic301@gmail.com>
2019-11-15 00:24:38 +0900

    Login Form & Post

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import Link from 'next/link';
-import Proptypes from "prop-types";
-import { Menu, Input, Button } from 'antd';
+import PropTypes from 'prop-types';
+import { Menu, Input, Row, Col } from 'antd';
+import LoginForm from './loginForm';
+import UserProfile from './UserProfile';
+const dummy = {
+  nickname: '최현준',
+  Post: [],
+  Followings: [],
+  Followers: [],
+  isLoggedIn: false,
+
+};
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <Menu mode="horizontal">
+     <Menu mode="horizontal">
         <Menu.Item key="home"><Link href="/"><a>노드버드</a></Link></Menu.Item>
         <Menu.Item key="profile"><Link href="/profile"><a>프로필</a></Link></Menu.Item>
         <Menu.Item key="mail">
           <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
         </Menu.Item>
       </Menu>
-      <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {dummy.isLoggedIn
+            ? <UserProfile />
+            : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+         
+        </Col>
+      </Row>
     </div>
   );
 };
 
 
-AppLayout.proptypes={
-  children:Proptypes.node
-}
+AppLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+
 export default AppLayout;
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
 
+const dummy = {
+  isLoggedIn: true,
+  imagePaths: [],
+  mainPosts: [{
+    User: {
+      id: 1,
+      nickname: '최현준',
+    },
+    content: '첫 번째 게시글',
+    img: 'https://cdn.pixabay.com/photo/2019/01/17/23/14/work-3938876_960_720.jpg',
+  }],
+};
 
 const Home = () => {
   return (
-    <>
-      
-        <div>Hello, Next!</div>
-    
-    </>
+    <div>
+      {dummy.isLoggedIn && <PostForm />}
+      {dummy.mainPosts.map((c) => {
+        return (
+          <PostCard key={c} post={c} />
+        );
+      })}
+    </div>
   );
 };
 
